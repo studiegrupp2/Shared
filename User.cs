f554aff9376b904cd800a98bd954b1c4029de1e1
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-01-24 12:02:00 +0100

    user funktionalitet

---
+++ User.cs
@@ -0,0 +1,19 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+
+public class User 
+{
+    [BsonId]
+    [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+    public string Id { get; set; }
+    
+    public string UserName { get; set; }
+    public string Password { get; set; }
+
+    public User(string userName, string password)
+    {
+        this.UserName = userName;
+        this.Password = password;
+    }
+}
