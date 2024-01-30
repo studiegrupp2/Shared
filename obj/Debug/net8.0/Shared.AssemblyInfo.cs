b8f3e7e84bad9aa4b4ba431b2297e97e7581824b
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-01-30 13:28:14 +0100

    fixing chat funtion

--- obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+453efe14100a50669502fc513b68467431d509d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac7e5c45fc8236dd1005b3d82723ea90fdc3769")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
