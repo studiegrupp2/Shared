8134f1293271c7a95059e991a46c3d982b58b996
IsakGunnardo <127835187+IsakGunnardo@users.noreply.github.com>
2024-02-01 10:09:10 +0100

    try/catch working and error handling

--- obj/Debug/net8.0/Shared.AssemblyInfo.cs
+++ obj/Debug/net8.0/Shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd097a2c0890a2f97a5c7bd16d559967319a3713")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228d908d5264828204f3318a9c7deeb42bbd117a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
